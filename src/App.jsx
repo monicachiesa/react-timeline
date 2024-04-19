505e945853b14d90dc7ec185e88a453d0714d376
monicachiesa <monica.chiesa@universo.univates.br>
2024-04-18 22:47:04 -0300

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+import { Header } from "./components/Header";
+import styles from "./App.module.css";
+
+import "./global.css";
+import { Sidebar } from "./components/Sidebar";
+
+function App() {
+  return (
+    <div>
+      <Header />
+
+      <div className="wrapper">
+        <Sidebar />
+        <main></main>
+      </div>
+    </div>
+  );
+}
+
+export default App;
